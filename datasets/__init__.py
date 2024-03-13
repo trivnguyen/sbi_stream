cc34e8a8c54b9dfacc6e091337e275d4f71a595d
Rutong Pei <rpei@andrew.cmu.edu>
2024-03-13 11:31:07 -0700

    preprocess_utils always return count; temporarily remove read_raw_dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -111,23 +111,16 @@ def read_process_dataset(
 
                 if num_bins > 0:
                     # bin the stream
-                    if frac:
-                        phi1_bin_centers, feat_mean, feat_stdv, feat_count = preprocess_utils.bin_stream(
-                        phi1_subsample, feat_subsample, num_bins=num_bins,
-                        phi1_min=phi1_min, phi1_max=phi1_max, count=True)
-
-                        feat_frac = feat_count/len(phi1)
-                    else: 
-                        phi1_bin_centers, feat_mean, feat_stdv = preprocess_utils.bin_stream(
+                    phi1_bin_centers, feat_mean, feat_stdv, feat_count = preprocess_utils.bin_stream(
                         phi1_subsample, feat_subsample, num_bins=num_bins,
                         phi1_min=phi1_min, phi1_max=phi1_max)
 
                     if len(phi1_bin_centers) == 0:
-                        print('hello')
                         continue
 
                     if frac: 
-                        x.append(np.concatenate([feat_mean, feat_stdv, feat_count, feat_frac], axis=1))
+                        feat_frac = feat_count / len(phi1)
+                        x.append(np.concatenate([feat_mean, feat_stdv, feat_frac], axis=1))
                     else: 
                         x.append(np.concatenate([feat_mean, feat_stdv], axis=1))
                     y.append(label)
@@ -146,61 +139,6 @@ def read_process_dataset(
     y = np.stack(y, axis=0)
 
     return x, y, t, padding_mask
-
-def read_raw_dataset(
-    data_dir: Union[str, Path], labels: List[str],
-    phi1_min: float = None, phi1_max: float = None,
-    num_datasets: int = 1
-):
-    """ Read raw data
-
-    Parameters
-    ----------
-    data_dir : str
-        Path to the directory containing the stream data.
-    labels : list of str
-        List of labels to use for the regression.
-    phi1_min : float, optional
-        Minimum value of phi1 to use. Default is None.
-    phi1_max : float, optional
-        Maximum value of phi1 to use. Default is None.
-    num_datasets : int, optional
-        Number of datasets to read in. Default is 1.
-    """
-
-    raw = []
-
-    for i in range(num_datasets):
-        label_fn = os.path.join(data_dir, f'labels.{i}.csv')
-        data_fn = os.path.join(data_dir, f'data.{i}.hdf5')
-
-        if os.path.exists(label_fn) & os.path.exists(data_fn):
-            print('Reading in data from {}'.format(data_fn))
-        else:
-            print('Dataset {} not found. Skipping...'.format(i))
-            continue
-
-        # read in the data and label
-        table = pd.read_csv(label_fn)
-        data, ptr = io_utils.read_dataset(data_fn, unpack=True)
-
-        # compute some derived labels
-        table = calculate_derived_properties(table)
-
-        loop = tqdm(range(len(table)))
-
-        for pid in loop:
-            loop.set_description(f'Processing pid {pid}')
-            phi1 = data['phi1'][pid]
-            phi2 = data['phi2'][pid]
-            pm1 = data['pm1'][pid]
-            pm2 = data['pm2'][pid]
-            vr = data['vr'][pid]
-            dist = data['dist'][pid]
-
-            raw.append(np.stack([phi1, phi2, pm1, pm2, vr, dist]))
-
-    return raw
 
 def prepare_dataloader(
     data: Tuple,
